5733cd38fc4b634cef823b769d548f897321bbc4
Kriti Sen Sharma <ksen@paradigm4.com>
2016-05-27 16:53:47 -0400

    Update plugin_scripts.sh

--- scripts/plugin_scripts.sh
+++ scripts/plugin_scripts.sh
@@ -3,13 +3,14 @@
 # First extract the library name from the lib*.so file, and verify it
 NAME=`ls lib*.so | sed "s/lib\(.*\).so/\1/"`
 echo $NAME
+DNAME="mydb"
 
 # Copy the script across the cluster
 for ip in $ALL; do sudo scp lib$NAME.so $ip:/opt/scidb/$SCIDB_VER/lib/scidb/plugins/ ; done
 
 # Restart the cluster (if needed; Also change to appropriate DBNAME as necessary)
-scidb.py stopall mydb
-scidb.py startall mydb
+scidb.py stopall $DBNAME
+scidb.py startall $DBNAME
 
 # Load the library
 iquery -aq "load_library('$NAME')"
